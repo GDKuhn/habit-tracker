5ee3201d41bb5ca856a26369ded64f0d06cf4bc1
Guilherme Kuhn <98616251+GDKuhn@users.noreply.github.com>
2023-01-19 16:40:47 -0300

    creating Hom,Header,HabitDay components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,35 +1,38 @@
-import './src/lib/dayjs';
+import "./src/lib/dayjs";
 
-import { StatusBar } from 'react-native';
-import { 
+import { StatusBar } from "react-native";
+
+import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
-  Inter_800ExtraBold
-} from '@expo-google-fonts/inter';
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
 
-import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_800ExtraBold
+    Inter_800ExtraBold,
   });
 
   if (!fontsLoaded) {
-    return (
-      <Loading />
-    );
+    return <Loading />;
   }
 
   return (
     <>
       <Home />
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </>
   );
 }
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        background: '#09090a',
+        background: "#09090a",
       },
       fontFamily: {
-        regular: 'Inter_400Regular',
-        semibold: 'Inter_600SemiBold',
-        bold: 'Inter_700Bold',
-        extrabold: 'Inter_800ExtraBold'
-      }
+        regular: "Inter_400Regular",
+        semibold: "Inter_600SemiBold",
+        bold: "Inter_700Bold",
+        extrabold: "Inter_800ExtraBold",
+      },
     },
   },
   plugins: [],
-}
+};
