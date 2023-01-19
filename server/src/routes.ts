585ab54730d09b64e9dcac49267089896a5020a9
Guilherme Kuhn <98616251+GDKuhn@users.noreply.github.com>
2023-01-19 11:55:41 -0300

    possibelHabits & completedHabits Routes added

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,76 @@
 import { prisma } from "./lib/prisma";
+import { z } from "zod";
 import { FastifyInstance } from "fastify";
+import dayjs from "dayjs";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const habits = await prisma.habit.findMany();
-    return habits;
+  app.post("/habits", async (req) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(req.body);
+
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
+  });
+
+  app.get("/day", async (req) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(req.query);
+
+    const parsedDate = dayjs(date).startOf("day");
+
+    const weekDay = parsedDate.get("day");
+
+    //todos hábitos possiveis
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    //todos hábitos completados
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
   });
 }
